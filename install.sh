f0391b1a953ed7916548c2c1f7a37af37434d3cd
ahhfzwl <ahhfzwl@qq.com>
2023-10-14 13:04:41 +0800

    Update install.sh
    Signed-off-by: ahhfzwl <ahhfzwl@qq.com>

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ rm -rf /root/sing-box/
 mkdir /root/sing-box/ && cd /root/sing-box/
 wget https://github.sock.cf/SagerNet/sing-box/releases/download/v1.5.3/sing-box-1.5.3-linux-amd64.tar.gz
 tar -xvf sing-box-*.tar.gz
-cp ./sing-box-*/sing-box /root/sing-box/sing-box
+cp ./sing-box-*/sing-box /root/sing-box/
 chmod +x /root/sing-box/sing-box
 rm -rf /root/sing-box/sing-box-*
 wget -O /root/sing-box/config.json https://raw.sock.cf/ahhfzwl/sing-box/dev-next/config.json
