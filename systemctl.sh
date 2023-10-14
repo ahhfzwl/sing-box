6b74a00b8fb4a24ba26574424fd7728d5afbacfb
ahhfzwl <ahhfzwl@qq.com>
2023-10-14 11:25:12 +0800

    Add files via upload
    Signed-off-by: ahhfzwl <ahhfzwl@qq.com>

---
+++ systemctl.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+while true
+do
+meta=$(curl -x socks5://127.0.0.1:1080 -s https://speed.cloudflare.com/meta --connect-timeout 2 --max-time 3 | awk -F\" '{print $22}')
+if [ "$meta" != "HKG" ]
+then
+	echo $meta......
+	systemctl restart sing-box
+	sleep 1
+else
+	echo $meta true
+	break
+fi
+done
