e3e023e5c92a51eccd3c84cee3e1909790a0c9d3
ahhfzwl <ahhfzwl@qq.com>
2023-10-14 13:12:12 +0800

    Update install.sh
    Signed-off-by: ahhfzwl <ahhfzwl@qq.com>

--- install.sh
+++ install.sh
@@ -1,12 +1,11 @@
 #!/bin/bash -e
 apt update && apt install -y openssh-server systemctl wget curl vim nano screen unzip
-rm -rf /root/sing-box/
 mkdir /root/sing-box/ && cd /root/sing-box/
 wget https://github.sock.cf/SagerNet/sing-box/releases/download/v1.5.3/sing-box-1.5.3-linux-amd64.tar.gz
 tar -xvf sing-box-*.tar.gz
 cp ./sing-box-*/sing-box /root/sing-box/
-chmod +x /root/sing-box/sing-box
 rm -rf /root/sing-box/sing-box-*
+chmod +x /root/sing-box/sing-box
 wget -O /root/sing-box/config.json https://raw.sock.cf/ahhfzwl/sing-box/dev-next/config.json
 wget -O /root/sing-box/systemctl.sh https://raw.sock.cf/ahhfzwl/sing-box/dev-next/systemctl.sh
 wget -O /etc/systemd/system/sing-box.service https://raw.sock.cf/ahhfzwl/sing-box/dev-next/sing-box.service
