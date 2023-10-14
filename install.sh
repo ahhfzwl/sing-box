c7480bfbfc0f3a1e1e0b8dc238b391dbcbe8ef8d
ahhfzwl <ahhfzwl@qq.com>
2023-10-14 11:35:52 +0800

    Update install.sh
    Signed-off-by: ahhfzwl <ahhfzwl@qq.com>

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
-apt update && apt install -y openssh-server systemctl wget curl vim nano screen
-mkdir /root/sing-box-warp/ && cd /root/sing-box-warp/
-wget -O /root/sing-box-warp/sing-box https://github.sock.cf/ahhfzwl/sing-box/releases/download/1/sing-box
-wget -O /root/sing-box-warp/config.json https://github.sock.cf/ahhfzwl/sing-box/blob/dev-next/config.json
-wget -O /root/sing-box-warp/systemctl.sh https://github.sock.cf/ahhfzwl/sing-box/blob/dev-next/systemctl.sh
-wget -O /etc/systemd/system/sing-box-warp.service https://github.sock.cf/ahhfzwl/sing-box/blob/dev-next/sing-box.service
-chmod +x /root/sing-box-warp/sing-box
+apt update && apt install -y openssh-server systemctl wget curl vim nano screen unzip
+mkdir /root/sing-box/ && cd /root/sing-box/
+wget https://github.sock.cf/SagerNet/sing-box/releases/download/v1.5.3/sing-box-1.5.3-linux-amd64.tar.gz
+unzip sing-box-*.zip
+cp ./sing-box-*/sing-box /root/sing-box/sing-box
+chmod +x /root/sing-box/sing-box
+wget -O /root/sing-box/config.json https://github.sock.cf/ahhfzwl/sing-box/blob/dev-next/config.json
+wget -O /root/sing-box/systemctl.sh https://github.sock.cf/ahhfzwl/sing-box/blob/dev-next/systemctl.sh
+wget -O /etc/systemd/system/sing-box.service https://github.sock.cf/ahhfzwl/sing-box/blob/dev-next/sing-box.service
 systemctl restart sing-box-warp # 重启服务
 systemctl enable sing-box-warp # 开机启动
