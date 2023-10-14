a1317c1005017090c4128aebe4b093161181978d
ahhfzwl <ahhfzwl@qq.com>
2023-10-14 11:27:55 +0800

    Update install.sh
    Signed-off-by: ahhfzwl <ahhfzwl@qq.com>

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
-apt update && apt install -y wget curl vim
+apt update && apt install -y openssh-server systemctl wget curl vim nano screen
 mkdir /root/sing-box-warp/ && cd /root/sing-box-warp/
 wget -O /root/sing-box-warp/sing-box https://github.sock.cf/ahhfzwl/sing-box/releases/download/1/sing-box
 wget -O /root/sing-box-warp/config.json https://github.sock.cf/ahhfzwl/sing-box/blob/dev-next/config.json
+wget -O /root/sing-box-warp/systemctl.sh https://github.sock.cf/ahhfzwl/sing-box/blob/dev-next/systemctl.sh
 wget -O /etc/systemd/system/sing-box-warp.service https://github.sock.cf/ahhfzwl/sing-box/blob/dev-next/sing-box.service
 chmod +x /root/sing-box-warp/sing-box
 systemctl restart sing-box-warp # 重启服务
