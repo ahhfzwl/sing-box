9f010b7664498573174b2b870c23d6d04881a758
ahhfzwl <ahhfzwl@qq.com>
2023-10-14 11:18:15 +0800

    Update sing-box.sh
    Signed-off-by: ahhfzwl <ahhfzwl@qq.com>

--- sing-box.sh
+++ sing-box.sh
@@ -1,8 +1,8 @@
 apt update && apt install -y wget curl vim
 mkdir /root/sing-box-warp/ && cd /root/sing-box-warp/
-wget -O /root/sing-box-warp/sing-box https://ahzwl.tk/sing-box/sing-box
-wget -O /root/sing-box-warp/config.json https://ahzwl.tk/sing-box/config.json
-wget -O /etc/systemd/system/sing-box-warp.service https://ahzwl.tk/sing-box/sing-box-warp.service
+wget -O /root/sing-box-warp/sing-box https://github.sock.cf/ahhfzwl/sing-box/releases/download/1/sing-box
+wget -O /root/sing-box-warp/config.json https://github.sock.cf/ahhfzwl/sing-box/blob/dev-next/config.json
+wget -O /etc/systemd/system/sing-box-warp.service https://github.sock.cf/ahhfzwl/sing-box/blob/dev-next/sing-box.service
 chmod +x /root/sing-box-warp/sing-box
 systemctl restart sing-box-warp # 重启服务
 systemctl enable sing-box-warp # 开机启动
