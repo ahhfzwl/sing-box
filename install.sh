69c29bee6933fac613e59d3bea34344fb49e45aa
ahhfzwl <ahhfzwl@qq.com>
2023-11-22 15:59:19 +0800

    Update install.sh
    Signed-off-by: ahhfzwl <ahhfzwl@qq.com>

--- install.sh
+++ install.sh
@@ -3,17 +3,50 @@
 case "$(uname -m)" in x86_64|amd64|x64)CPU=amd64;;i386|i686)CPU=386;;armv8|arm64|aarch64)CPU=arm64;;armv6l|armv7|armv7l)CPU=armv7;;*)echo "none CPU" && exit;esac
 VERSION=$(curl https://api.github.com/repos/SagerNet/sing-box/releases | grep -oP "sing-box-\d+\.\d+\.\d+-linux-$CPU"| sort -Vru | head -n 1)
 curl -Lo /tmp/$VERSION.tar.gz https://github.sock.cf/SagerNet/sing-box/releases/latest/download/$VERSION.tar.gz
-tar -xzf /tmp/$VERSION.tar.gz -C /tmp
+tar -xzf /tmp/$VERSION.tar.gz -C /tmp/
+cp /tmp/sing-box-*/sing-box /usr/local/bin/
+chmod +x /usr/local/bin/sing-box
+rm -rf /tmp/sing-box-*
 
+mkdir /etc/sing-box/
+cat <<'TEXT' > /etc/sing-box/config.json
+{
+  "inbounds":[
+    {
+      "type": "vmess",
+      "listen": "::",
+      "listen_port": 8080,
+      "users": [
+        {
+          "name": "vmess",
+          "uuid": "11112222-3333-4444-aaaa-bbbbccccdddd"
+        }
+      ],
+      "transport": {
+        "type": "ws",
+        "path": "/vm",
+        "early_data_header_name": "Sec-WebSocket-Protocol"
+      }
+    }
+  ],
+  "outbounds":[{"type":"direct"}]
+}
+TEXT
 
-apt update && apt install -y openssh-server systemctl wget curl vim nano screen unzip htop
-mkdir /root/sing-box/ && cd /root/sing-box/
+cat <<'TEXT' > /etc/systemd/system/sing-box.service
+[Unit]
+Description=sing-box
+After=network.target
 
-tar -xvf sing-box-*.tar.gz
-cp ./sing-box-*/sing-box /root/sing-box/
-rm -rf /root/sing-box/sing-box-*
-chmod +x /root/sing-box/sing-box
-wget -O /root/sing-box/config.json https://raw.sock.cf/ahhfzwl/sing-box/dev-next/config.json
-wget -O /root/sing-box/systemctl.sh https://raw.sock.cf/ahhfzwl/sing-box/dev-next/systemctl.sh
-wget -O /etc/systemd/system/sing-box.service https://raw.sock.cf/ahhfzwl/sing-box/dev-next/sing-box.service
-systemctl enable sing-box # 开机启动
+[Install]
+WantedBy=multi-user.target
+
+[Service]
+Type=simple
+WorkingDirectory=/usr/local/bin/
+ExecStart=/usr/local/bin/sing-box run -c /etc/sing-box/config.json
+Restart=always
+TEXT
+
+systemctl enable sing-box
+systemctl restart sing-box
