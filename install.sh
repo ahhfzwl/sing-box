4ea803ddd68996e545c0da82682f5fca4614cb68
ahhfzwl <ahhfzwl@qq.com>
2023-11-22 15:32:40 +0800

    Update install.sh
    Signed-off-by: ahhfzwl <ahhfzwl@qq.com>

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/bash
 
-case "$(uname -m)" in x86_64|amd64|x64)CPU=amd64;;i386|i686)CPU=386;;armv8|arm64|aarch64)CPU=arm64;;armv6l|armv7|armv7l)CPU=armv7;;*)echo "none CPU" && exit;esac && echo $CPU
-VERSION=$(curl https://api.github.com/repos/SagerNet/sing-box/releases | grep -oP "sing-box-\d+\.\d+\.\d+-linux-$CPU"| sort -Vru | head -n 1) && echo "$VERSION"
-curl -o /tmp/$VERSION.tar.gz https://github.sock.cf/SagerNet/sing-box/releases/latest/download/$VERSION.tar.gz
+case "$(uname -m)" in x86_64|amd64|x64)CPU=amd64;;i386|i686)CPU=386;;armv8|arm64|aarch64)CPU=arm64;;armv6l|armv7|armv7l)CPU=armv7;;*)echo "none CPU" && exit;esac
+VERSION=$(curl https://api.github.com/repos/SagerNet/sing-box/releases | grep -oP "sing-box-\d+\.\d+\.\d+-linux-$CPU"| sort -Vru | head -n 1)
+curl -Lo /tmp/$VERSION.tar.gz https://github.sock.cf/SagerNet/sing-box/releases/latest/download/$VERSION.tar.gz
 
 apt update && apt install -y openssh-server systemctl wget curl vim nano screen unzip htop
 mkdir /root/sing-box/ && cd /root/sing-box/
