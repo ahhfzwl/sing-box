94e142c1700987d3350c515f55b43714d6384a5c
ahhfzwl <ahhfzwl@qq.com>
2023-11-22 16:07:45 +0800

    Update systemctl.sh
    Signed-off-by: ahhfzwl <ahhfzwl@qq.com>

--- systemctl.sh
+++ systemctl.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 while true
 do
-meta=$(curl -x socks5://127.0.0.1:1080 -s https://speed.cloudflare.com/meta --connect-timeout 2 --max-time 3 | awk -F\" '{print $22}')
+meta=$(curl -x socks5://127.0.0.1:1080 -s https://speed.cloudflare.com/meta -m 3 | awk -F\" '{print $22}')
 if [ "$meta" != "HKG" ]
 then
 	echo $meta......
