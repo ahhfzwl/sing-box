b1b776f2656e17d942d8658561806d067e33a410
ahhfzwl <ahhfzwl@qq.com>
2023-11-23 08:58:45 +0800

    Update sing-box.sh
    Signed-off-by: ahhfzwl <ahhfzwl@qq.com>

--- sing-box.sh
+++ sing-box.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/bash
 
+install(){
 case "$(uname -m)" in x86_64|amd64|x64)CPU=amd64;;i386|i686)CPU=386;;armv8|arm64|aarch64)CPU=arm64;;armv6l|armv7|armv7l)CPU=armv7;;*)echo "none CPU" && exit;esac
 VERSION=$(curl https://api.github.com/repos/SagerNet/sing-box/releases | grep -oP "sing-box-\d+\.\d+\.\d+-linux-$CPU"| sort -Vru | head -n 1)
 curl -Lo /tmp/$VERSION.tar.gz https://github.sock.cf/SagerNet/sing-box/releases/latest/download/$VERSION.tar.gz
@@ -50,3 +51,4 @@ TEXT
 
 systemctl enable sing-box
 systemctl restart sing-box
+}
