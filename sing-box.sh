b8de456483a795054e097082b4ffedc2435f8f22
ahhfzwl <ahhfzwl@qq.com>
2023-11-23 12:43:16 +0800

    Update sing-box.sh
    Signed-off-by: ahhfzwl <ahhfzwl@qq.com>

--- sing-box.sh
+++ sing-box.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-apk(){
+apk() {
   APK=
   for i in wget curl cron nano screen
   do
@@ -15,7 +15,7 @@ apk(){
   fi
 }
 
-add(){
+add() {
   case "$(uname -m)" in x86_64|amd64|x64)CPU=amd64;;i386|i686)CPU=386;;armv8|arm64|aarch64)CPU=arm64;;armv6l|armv7|armv7l)CPU=armv7;;*)echo "none CPU" && exit;esac
   VERSION=$(curl https://api.github.com/repos/SagerNet/sing-box/releases | grep -oP "sing-box-\d+\.\d+\.\d+-linux-$CPU"| sort -Vru | head -n 1)
   curl -Lo /tmp/$VERSION.tar.gz https://github.sock.cf/SagerNet/sing-box/releases/latest/download/$VERSION.tar.gz
@@ -68,7 +68,7 @@ add(){
   systemctl restart sing-box
 }
 
-del(){
+del() {
   systemctl stop sing-box
   systemctl disable sing-box
   rm -rf /etc/systemd/system/sing-box.service
