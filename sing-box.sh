fac7670e9ba0ad29fd29a0424974d60a21084632
ahhfzwl <ahhfzwl@qq.com>
2023-12-16 16:24:32 +0800

    Update sing-box.sh
    Signed-off-by: ahhfzwl <ahhfzwl@qq.com>

--- sing-box.sh
+++ sing-box.sh
@@ -16,7 +16,7 @@ apk() {
 }
 
 add() {
-case "$(uname -m)" in x86_64|amd64|x64)CPU=amd64;;i386|i686)CPU=386;;armv8|arm64|aarch64)CPU=arm64;;armv6l|armv7|armv7l)CPU=armv7;;*)echo "none CPU" && exit;esac
+case `uname -m` in x86_64|amd64|x64)CPU=amd64;;i386|i686)CPU=386;;armv8|arm64|aarch64)CPU=arm64;;armv6l|armv7|armv7l)CPU=armv7;;*)echo CPU???;exit;esac
 VER=$(curl https://api.github.com/repos/SagerNet/sing-box/releases | grep -oP "sing-box-\d+\.\d+\.\d+-linux-$CPU"| sort -Vru | head -n 1)
 curl -Lo /tmp/$VERSION.tar.gz https://github.sock.cf/SagerNet/sing-box/releases/latest/download/$VER.tar.gz
 tar -xzf /tmp/$VERSION.tar.gz -C /tmp/
