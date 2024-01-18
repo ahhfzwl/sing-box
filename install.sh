7b0b5903da5d5e94413de855bfb35538167b476c
ahhfzwl <ahhfzwl@qq.com>
2024-01-18 16:36:44 +0800

    Update install.sh
    Signed-off-by: ahhfzwl <ahhfzwl@qq.com>

--- install.sh
+++ install.sh
@@ -32,14 +32,10 @@ install_sing-box() {
   echo "download /etc/init.d/sing-box"
   curl -Lso /etc/init.d/sing-box https://raw.sock.cf/ahhfzwl/sing-box/main/sing-box
   chmod +x /etc/init.d/sing-box
-  case `uname -m` in x86_64|amd64|x64)CPU=amd64;;i386|i686)CPU=386;;armv8|arm64|aarch64)CPU=arm64;;armv6l|armv7|armv7l)CPU=armv7;;*)echo CPU???;exit;esac
-  VER=$(curl -s https://api.github.com/repos/SagerNet/sing-box/releases | grep -oP "sing-box-\d+\.\d+\.\d+-linux-$CPU"| sort -Vru | head -n 1)
-  echo "download $VER.tar.gz"
-  curl -Lo /tmp/$VER.tar.gz https://github.com/SagerNet/sing-box/releases/latest/download/$VER.tar.gz
-  tar -xzf /tmp/$VER.tar.gz -C /tmp/
-  cp /tmp/sing-box-*/sing-box /usr/local/bin/sing-box
-  chmod +x /usr/local/bin/sing-box
-  rm -rf /tmp/sing-box-*
+  wget "$(curl -s https://api.github.com/repos/SagerNet/sing-box/releases/latest | grep -o "https://github.com/SagerNet/sing-box/releases/download/.*/sing-box-.*-linux-$(uname -m | sed 's/x86_/amd/; s/aarch/arm/').tar.gz" | sort -V | head -n 1)"
+  tar -zxvf sing-box-*
+  mv ./sing-box-*/sing-box /usr/local/bin/
+  rm -rf sing-box-*
   systemctl daemon-reload
   systemctl restart sing-box
   systemctl enable sing-box
@@ -49,8 +45,8 @@ remove_sing-box() {
   echo "disable sing-box"
   systemctl disable sing-box
   echo "stop sing-box"
-  pkill sing-box
   systemctl stop sing-box
+  pkill sing-box
   echo "del /etc/systemd/system/sing-box.service"
   rm -rf /etc/systemd/system/sing-box.service
   echo "del /etc/sing-box"
@@ -59,6 +55,7 @@ remove_sing-box() {
   rm -rf /usr/local/bin/sing-box
   echo "del /etc/init.d/sing-box"
   rm -rf /etc/init.d/sing-box
+  systemctl daemon-reload
 }
 
 case $SING in
